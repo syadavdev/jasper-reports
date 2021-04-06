e0cbcc50d0ee27b69c485daf0da0b61a00d3bd0e
Sandeep Yadav <sandi10.96@gmail.com>
2021-04-06 17:47:43 +0530

    initial commit

---
+++ src/main/java/com/sandi/jasperreports/JasperReportsApplication.java
@@ -0,0 +1,13 @@
+package com.sandi.jasperreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandi/jasperreports/JasperReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandi.jasperreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
