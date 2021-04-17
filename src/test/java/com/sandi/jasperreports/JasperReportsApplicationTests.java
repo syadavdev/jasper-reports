e99e171040650cb3946252d2f1b799bf62704b4f
Sandeep Yadav <sandi10.96@gmail.com>
2021-04-18 02:01:35 +0530

    Add H2 database dependency for testing

--- src/test/java/com/sandi/jasperreports/JasperReportsApplicationTests.java
+++ src/test/java/com/sandi/jasperreports/JasperReportsApplicationTests.java
@@ -2,12 +2,14 @@ package com.sandi.jasperreports;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class JasperReportsApplicationTests {
+@ActiveProfiles("test")
+public class JasperReportsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
